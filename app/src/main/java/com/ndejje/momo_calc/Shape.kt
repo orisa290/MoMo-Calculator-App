a8eaf87427b5bc2156b6008d16c73bc9d57fca1b
orisa290 <orisa3018@gmail.com>
2026-04-07 08:47:28 +0300

    Module 5B.2: Create Shape.kt with rounded corner shapes

---
+++ app/src/main/java/com/ndejje/momo_calc/Shape.kt
@@ -0,0 +1,13 @@
+package com.ndejje.momo_calc
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Shapes
+import androidx.compose.ui.unit.dp
+
+val MoMoShapes = Shapes(
+    extraSmall = RoundedCornerShape(4.dp),
+    small      = RoundedCornerShape(8.dp),
+    medium     = RoundedCornerShape(16.dp),
+    large      = RoundedCornerShape(24.dp),
+    extraLarge = RoundedCornerShape(28.dp)
+)
